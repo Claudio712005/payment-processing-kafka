5860013986ace3167a3843a2e5c562a5f773124e
Claudio712005 <clausilvaaraujo11@gmail.com>
2026-09-15 22:04:13 -0300

    feat: adicionando novas deps. para utilizar o avro

--- payment-srv-producer/build.gradle.kts
+++ payment-srv-producer/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "2.3.21"
     id("org.springframework.boot") version "4.1.1"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.bakdata.avro") version "2.0.0"
 }
 
 group = "com.clau"
@@ -17,13 +18,19 @@ java {
 
 repositories {
     mavenCentral()
+    // O KafkaAvroSerializer/Deserializer da Confluent NÃO está no Maven Central.
+    maven { url = uri("https://packages.confluent.io/maven/") }
 }
 
 extra["datasourceMicrometerVersion"] = "2.2.1"
+extra["confluentVersion"] = "7.9.1"
+extra["avroVersion"] = "1.12.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-kafka")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.apache.avro:avro:${property("avroVersion")}")
+    implementation("io.confluent:kafka-avro-serializer:${property("confluentVersion")}")
     implementation("net.ttddyy.observation:datasource-micrometer-spring-boot")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("tools.jackson.module:jackson-module-kotlin")
